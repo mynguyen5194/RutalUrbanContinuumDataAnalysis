b59aabf6b5b90f21665b0853f7359b50d3f07d93
alonanko <alon.ankonina@gmail.com>
2014-11-10 15:13:35 -0800

    Update County.h

--- County.h
+++ County.h
@@ -15,7 +15,7 @@ private:
 public:
     County() {key = 0; state = ""; countyName = ""; population = 0; rucc = 0;}
 
-    int getKey() {return "I am going to change my master";}
+    int getKey() {return key;}
     int getPopulation() {return population;}
     string getState() {return state;}
     string getRUCC() {return rucc;}
