e934cb723a201651d91cce381a25dccfc664ed1d
My Nguyen <mynguyen5194@gmail.com>
2014-11-10 14:44:48 -0800

    Team Project

---
+++ County.h
@@ -0,0 +1,30 @@
+#ifndef COUNTY_H
+#define COUNTY_H
+
+#include <iostream>
+#include <string>
+
+class County {
+private:
+    int key;
+    string state;
+    string countyName;
+    int population;
+    int rucc;
+
+public:
+    County() {key = 0; state = ""; countyName = ""; population = 0; rucc = 0;}
+
+    int getKey() {return key;}
+    int getPopulation() {return population;}
+    string getState() {return state;}
+    string getRUCC() {return rucc;}
+    string getcountyName() {return countyName;}
+
+    void setKey(int aKey) {key = aKey;}
+    void setPopulation(int aPop) {population = aPop;}
+    void setState(string aState) {state = aState;}
+    void setCountyName(string aCounty) {countyName = aCounty;}
+    void setRucc (string aRucc) {rucc = aRucc;}
+}
+#endif
---
+++ main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  TeamProject
+//
+//  Created by My Nguyen on 11/10/14.
+//  Copyright (c) 2014 MyNguyen. All rights reserved.
+//
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    
+    return 0;
+}
+
+
